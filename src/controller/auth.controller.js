50fd1af8ba2a9cf9d9482fe313c94e95e7ff2a4c
joydeepdev <joydeep6123@gmail.com>
2026-04-15 17:31:41 +0530

    login with access token only

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -8,10 +8,40 @@ export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   const existingUser = await findUserByEmail(email);
-  if (existingUser) return ApiError.badRequest('User already exists');
+  if (existingUser) throw ApiError.badRequest('User already exists');
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await createUser(username, email, hashedPassword);
   ApiResponse.created(res, 'user created successfully', user);
 };
 
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  //checking user
+  const user = await findUserByEmail(email);
+  if (!user) throw ApiError.badRequest('Invalid credentials');
+
+  //checking password
+  const isMatch = await bcrypt.compare(password, user.password_hash);
+  if (!isMatch) throw ApiError.badRequest('Invalid credentials');
+
+  //generate access token
+  const accessToken = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.JWT_ACCESS_TOKEN,
+    { expiresIn: '15m' },
+  );
+  //send access token
+  ApiResponse.ok(res, 'logged in succssfully', {
+    accessToken,
+    user: {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { register } from '../controller/auth.controller.js';
+import { login, register } from '../controller/auth.controller.js';
 const router = Router();
 
 router.post('/register', register);
+router.post('/login',login)
 
 export default router;
